a761d19348a1383b8fa1dd80d7e5bd62cc621457
Adam Hany <adam.afify13@gmail.com>
2026-03-28 01:31:28 +0200

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, Extension
 
-# Define the C++ extension
+
 fast_join_module = Extension(
     'vibe_tui.managers.join', 
-    # Match the filename in your tree: join.c++
     sources=['vibe_tui/managers/join.c++'], 
     extra_compile_args=['-O3', '-fPIC'], 
 )
